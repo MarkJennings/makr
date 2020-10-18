301ea7dfb16f76b44831a7c67793eeae7ffbb30b
Mark Jennings <markjennings97@gmail.com>
2020-10-18 11:17:57 -0500

    Make landscape default

--- mobile.js
+++ mobile.js
@@ -2,7 +2,57 @@
 // Mobile detection: https://developer.mozilla.org/en-US/docs/Web/HTTP/Browser_detection_using_the_user_agent
 // Device orientation: https://davidwalsh.name/orientation-change
 // JS variables to CSS: https://css-tricks.com/updating-a-css-variable-with-javascript
-/*
+
+// True if mobile browser or device detected
+var mobile = (/Mobi|Android/i.test(navigator.userAgent));
+let root = document.documentElement;
+
+function makePortrait() {
+    root.style.setProperty('--titleSize', 150 + "%");
+    root.style.setProperty('--textSize', 100 + "%");
+    root.style.setProperty('--pWidth', 50 + "%");
+    root.style.setProperty('--bHeight', 7.5 + "vh");
+}
+
+function makeLandscape() {
+    root.style.setProperty('--titleSize', 170 + "%");
+    root.style.setProperty('--textSize', 105 + "%");
+    root.style.setProperty('--pWidth', 33.33 + "%");
+    root.style.setProperty('--bHeight', 15 + "vh");
+}
+
+if (mobile) {
+    // Initialize media query list monitoring orientation
+    var mql = window.matchMedia("(orientation: portrait)");
+
+    // If there are matches, we're in portrait
+    if(mql.matches) {  
+        // Portrait orientation
+        makePortrait();
+    } 
+    else {  
+        // Landscape orientation
+        makeLandscape();
+    }
+
+    // Add a media query change listener
+    mql.addListener(function(m) {
+        if(m.matches) {
+            // Changed to portrait
+            makePortrait();
+        }
+        else {
+            // Changed to landscape
+            makeLandscape();
+        }
+    });
+}
+else {
+    // Default to landscape
+    makeLandscape();
+}
+
+/* DEPRECATED
 function mobileScale() {
     // True if mobile browser or device detected
     var mobile = (/Mobi|Android/i.test(navigator.userAgent));
@@ -26,46 +76,3 @@ function mobileScale() {
 // Run the function before page starts loading!
 mobileScale();
 */
-
-// True if mobile browser or device detected
-var mobile = (/Mobi|Android/i.test(navigator.userAgent));
-let root = document.documentElement;
-
-function makePortrait() {
-    root.style.setProperty('--titleSize', 150 + "%");
-    root.style.setProperty('--textSize', 100 + "%");
-    root.style.setProperty('--pWidth', 50 + "%");
-    root.style.setProperty('--bHeight', 7.5 + "vh");
-}
-function makeLandscape() {
-    root.style.setProperty('--titleSize', 170 + "%");
-    root.style.setProperty('--textSize', 105 + "%");
-    root.style.setProperty('--pWidth', 33.33 + "%");
-    root.style.setProperty('--bHeight', 15 + "vh");
-}
-
-if (mobile) {
-    // Initialize media query list monitoring orientation
-    var mql = window.matchMedia("(orientation: portrait)");
-
-    // If there are matches, we're in portrait
-    if(mql.matches) {  
-        // Portrait orientation
-        makePortrait();
-    } else {  
-        // Landscape orientation
-        makeLandscape();
-    }
-
-    // Add a media query change listener
-    mql.addListener(function(m) {
-        if(m.matches) {
-            // Changed to portrait
-            makePortrait();
-        }
-        else {
-            // Changed to landscape
-            makeLandscape();
-        }
-    });
-}
