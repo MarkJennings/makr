2b20fe2425c2708b72f3930e7af5abfd338ebe55
Mark Jennings <markjennings97@gmail.com>
2021-01-26 17:20:01 -0600

    Improved mobile detection JS syntax

--- mobile.js
+++ mobile.js
@@ -3,8 +3,7 @@
 // Device orientation: https://davidwalsh.name/orientation-change
 // JS variables to CSS: https://css-tricks.com/updating-a-css-variable-with-javascript
 
-// True if mobile browser or device detected
-var mobile = (/Mobi|Android/i.test(navigator.userAgent));
+// Access handle to html elements
 let root = document.documentElement;
 
 function makePortrait() {
@@ -22,34 +21,29 @@ function makeLandscape() {
     root.style.setProperty('--picWidth', 50 + "%");
 }
 
-if (mobile) {
+// True if mobile browser or device detected
+var is_mobile = (/Mobi|Android/i.test(navigator.userAgent));
+
+if (is_mobile)
+{
     // Initialize media query list monitoring orientation
     var mql = window.matchMedia("(orientation: portrait)");
     // If it matches, we're in portrait
-    if(mql.matches) {  
-        // Portrait orientation
+    if(mql.matches)
         makePortrait();
-    } 
-    else {  
-        // Landscape orientation
+    else
         makeLandscape();
-    }
     // Add a media query change listener
-    mql.addListener(function(m) {
-        if(m.matches) {
-            // Changed to portrait
+    mql.addListener(function(m){
+        if(m.matches)
             makePortrait();
-        }
-        else {
-            // Changed to landscape
+        else
             makeLandscape();
-        }
     });
 }
-else {
-    // Default to landscape
+// Default to landscape
+else
     makeLandscape();
-}
 
 /* DEPRECATED
 function mobileScale() {
