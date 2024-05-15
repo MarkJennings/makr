76c810ed449e1a4824b548bb755139d3f0e48508
Mark Jennings <markjennings97@gmail.com>
2024-05-14 22:34:20 -0600

    Made CSS more responsive, especially for ultrawide monitors

--- mobile.js
+++ mobile.js
@@ -8,14 +8,12 @@ let root = document.documentElement;
 
 function makePortrait() {
     root.style.setProperty('--titleSize', 150 + "%");
-    root.style.setProperty('--textSize', 100 + "%");
     root.style.setProperty('--pWidth', 50 + "%");
     root.style.setProperty('--bHeight', 7.5 + "vh");
     root.style.setProperty('--picWidth', 65 + "%");
 }
 function makeLandscape() {
     root.style.setProperty('--titleSize', 170 + "%");
-    root.style.setProperty('--textSize', 100 + "%");
     root.style.setProperty('--pWidth', 33 + "%");
     root.style.setProperty('--bHeight', 10 + "vh");
     root.style.setProperty('--picWidth', 50 + "%");
@@ -44,28 +42,3 @@ if (is_mobile)
 // Default to landscape
 else
     makeLandscape();
-
-/* DEPRECATED
-function mobileScale() {
-    // True if mobile browser or device detected
-    var mobile = (/Mobi|Android/i.test(navigator.userAgent));
-    let root = document.documentElement;
-    // Mobile portrait orientation
-    if (mobile && Math.abs(window.orientation) !== 90) {
-        root.style.setProperty('--titleSize', 150 + "%");
-        root.style.setProperty('--textSize', 100 + "%");
-        root.style.setProperty('--pWidth', 50 + "%");
-        root.style.setProperty('--bHeight', 7.5 + "vh");
-    }
-    // Normal style settings
-    else {
-        root.style.setProperty('--titleSize', 170 + "%");
-        root.style.setProperty('--textSize', 105 + "%");
-        root.style.setProperty('--pWidth', 33.33 + "%");
-        root.style.setProperty('--bHeight', 15 + "vh");
-    }
-    window.alert(window.orientation);
-}
-// Run the function before page starts loading!
-mobileScale();
-*/
